69b2f239082498a5fb2ca6485362a5e747e28937
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-22 12:30:10 -0500

    minor updates

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const puppeteer = require("puppeteer");
+const puppeteerExtra = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
 
+const { executablePath } = require("puppeteer");
 async function main() {
+  puppeteerExtra.use(pluginStealth());
   try {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch(
+      { headless: false },
+      { executablePath: executablePath() }
+    );
     const page = await browser.newPage();
 
     // Navigate the page to a URL
     console.log("Opening web page...");
 
-    await page.goto("https://www.indeed.com");
+    await page.goto("https://www.indeed.com", {
+      waitUntil: "load",
+      timeout: 10000,
+    });
 
     // Set screen size
     console.log("Changing screen size...");
@@ -26,29 +36,65 @@ async function main() {
 
     console.log("Typing into search box...");
     const searchBoxSelector = "#text-input-what";
+    await page.waitForSelector(searchBoxSelector, {
+      visible: true,
+      timeout: 10000,
+    });
+    // type into search bar criteria
     await page.locator(searchBoxSelector).fill("Entry level Software engineer");
 
-    // Wait and click on the search button
+    // Wait for selector
     const searchResultSelector = ".yosegi-InlineWhatWhere-primaryButton";
     console.log("Clicking search button...");
+    await page.waitForSelector(searchResultSelector, {
+      visible: true,
+    });
+    // clicking on selector
     await page.locator(searchResultSelector).click();
-
+    await page.waitForNavigation({
+      waitUntil: "load",
+      timeout: 10000,
+    });
     // Wait for the search results to load
     console.log("Waiting for search results...");
-    await page.waitForNavigation();
+
+    // waiting
+    delay(4000000);
 
     // Click on experience level button
+
     const experienceButton = "#filter-explvl";
     console.log("Clicking experience level button...");
-    await page.waitForSelector(experienceButton);
+    await page.waitForSelector(experienceButton, {
+      visible: true,
+      timeout: 10000,
+    });
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
+    delay(4000000);
     await page.click(experienceButton);
-
+    console.log("click");
     // Wait for the experience level filter options to load
-
+    //TODO use evaluate to click on button posible solution change class or click
+    // @ use this class="yosegi-FilterPill-pillList css-zhqt0z eu4oa1w0" parent container
     // await browser.close();
   } catch (error) {
     console.log(error);
   }
+}
+
+function delay(time) {
+  console.log("slowing down a bit");
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
 }
 
 main();
